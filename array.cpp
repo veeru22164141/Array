3c74119a50c6424e301a5bb05df2127f6e0cf3cb
virendra yadav <virendra0103yadav@gmail.com>
2024-10-06 15:22:57 +0530

    modifide the maxIndexDiff2

--- array.cpp
+++ array.cpp
@@ -1,4 +1,6 @@
 #include "array.h"
+#include <algorithm>
+using namespace std;
 int Array::maxIndexDiff(std::vector<int> arr) {
     int n = arr.size();
     int maxDiff = -1;
@@ -11,4 +13,43 @@ int Array::maxIndexDiff(std::vector<int> arr) {
         }
     }
     return maxDiff;
+}
+
+int maxIndexDiff2(std::vector<int> arr) {
+     int n = arr.size();
+        if (n == 1) {
+            return 0;
+        }
+        int maxDiff;
+        int i, j;
+
+        int *LMin = new int[n];
+        int *RMax = new int[n];
+
+        // Constructing LMin[] such that LMin[i] stores the minimum value
+        // from (arr[0], arr[1], ... arr[i]).
+        LMin[0] = arr[0];
+        for (i = 1; i < n; ++i)
+            LMin[i] = min(arr[i], LMin[i - 1]);
+
+        // Constructing RMax[] such that RMax[j] stores the maximum value
+        // from (arr[j], arr[j+1], ..arr[n-1]).
+        RMax[n - 1] = arr[n - 1];
+        for (j = n - 2; j >= 0; --j)
+            RMax[j] = max(arr[j], RMax[j + 1]);
+
+        i = 0, j = 0, maxDiff = -1;
+        // Traversing both arrays from left to right to find optimum j-i.
+        // This process is similar to merge() of MergeSort.
+        while (j < n && i < n) {
+            if (LMin[i] <= RMax[j]) {
+                // Updating the maximum difference.
+                maxDiff = max(maxDiff, j - i);
+                j = j + 1;
+            } else
+                i = i + 1;
+        }
+        // returning the maximum difference.
+        return maxDiff;
+    
 }
